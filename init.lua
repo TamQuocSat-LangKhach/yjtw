bcc896c919da7a5cbf9bedd9c744fd73acc6deeb
notify <notify-ctrl@qq.com>
2024-01-26 15:47:38 +0800

    大包翻译

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 local yjtw2013 = require "packages/yjtw/yjtw2013"
 local yjtw2017 = require "packages/yjtw/yjtw2017"
 
+Fk:loadTranslationTable{ ["yjtw"] = "台版一将成名" }
+
 return {
   yjtw2013,
   yjtw2017,
